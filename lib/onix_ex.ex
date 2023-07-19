51dd9deb82ac3f7d3767814e4fd5cd557ed8b9da
Anderson Konzen <anderson@anderkonzen.com>
2023-07-19 00:11:06 -0300

    Bootstrap elixir project

---
+++ lib/onix_ex.ex
@@ -0,0 +1,18 @@
+defmodule OnixEx do
+  @moduledoc """
+  Documentation for `OnixEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OnixEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/onix_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule OnixExTest do
+  use ExUnit.Case
+  doctest OnixEx
+
+  test "greets the world" do
+    assert OnixEx.hello() == :world
+  end
+end
